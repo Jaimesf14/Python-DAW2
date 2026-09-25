f8bb17f1d499b496dd1fcb14f2a241249a7babe4
Jaimesf14 <jaimesf3r@gmail.com>
2026-09-25 10:19:23 +0200

    Pruebas dia1 (sumas, restas)

---
+++ prueba1.py
@@ -0,0 +1,18 @@
+#suma enteros
+num1 = int(input("Introduzca el primer número: "))
+num2 = int(input("Introduzca el segundo número: "))
+suma = num1 + num2
+print(f"La suma de {num1} y {num2} es {suma}")
+#suma decimales
+num1 = float(input("Introduzca el primer número: "))
+num2 = float(input("Introduzca el segundo número: "))
+sumadec = num1 + num2
+print(f"La suma de {num1} y {num2} es {sumadec}")
+
+#resta enteros
+num1 = int(input("Introduzca el primer número: "))
+num2 = int(input("Introduzca el segundo número: "))
+resta = num1 - num2
+print(f"La resta de {num1} y {num2} es {resta}")
+
+
